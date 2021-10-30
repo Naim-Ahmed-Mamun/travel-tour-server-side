0511ec4dea6e2d954855aa67865b9d5e8a9feca2
Naim-Ahmed-Mamun <naimahmod852@gmail.com>
2021-10-30 18:16:13 +0600

    added some api

--- index.js
+++ index.js
@@ -14,63 +14,106 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h3xh8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect()
         const database = client.db('Travels')
         const destinationCollection = database.collection('Destinations');
         const bookingCollection = database.collection('orderBooking')
 
         // GET API
-        app.get('/destinations',async(req,res) => {
-            const cursor = destinationCollection.find({})
+        app.get('/allDestinations', async (req, res) => {
+            const cursor = destinationCollection.find({});
+            const result = await cursor.toArray();
+            // console.log(result);
+            res.send(result)
+        })
+        // GET API
+        app.get('/destinations', async (req, res) => {
+            const cursor = destinationCollection.find().limit(6);
             const result = await cursor.toArray();
             // console.log(result);
             res.send(result)
         })
 
         // GET SPECIFIC ONE ITEM
-        app.get('/orderBooking/:id',async(req,res) => {
+        app.get('/bookingOrder/:id', async(req, res) => {
             const id = req.params.id;
-            const query = {_id:ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const result = await destinationCollection.findOne(query);
-            console.log(result,id);
+            // console.log(result, id);
             res.json(result)
         })
-        // GET order Booking
-        app.get('/orderBooking/:email',async(req,res) => {
+
+        // POST API
+        app.post('/addDestination', async (req, res) => {
+            const destination = req.body;
+            const result = await destinationCollection.insertOne(destination);
+            console.log('post hit', result);
+            res.json(result)
+        });
+         // GET order Booking
+         app.get('/userBooking/:email', async (req, res) => {
             const email = req.params.email;
-            // console.log(email);
             const query = {email:email}
             const cursor = bookingCollection.find(query)
             const result = await cursor.toArray();
-            console.log('post hitting add',result);
-            res.send(result)
+            // console.log('post hitting add',result);
+            res.json(result)
+        })
+        // GET MANGE ALL USERS
+        app.get('/bookingAllUsers',async(req,res) => {
+            const result = await bookingCollection.find({}).toArray();
+            res.json(result)
         })
         // POST API
-        app.post('/addDestination',async(req,res) => {
-            const destination = req.body;
-            const result = await destinationCollection.insertOne(destination);
-            console.log('post hit',result);
-            res.json(result)
-        });
-        // POST API
-        app.post('/bookingOrder',async(req,res) => {
+        app.post('/bookingOrder', async (req, res) => {
             const bookingOrder = req.body;
             const result = await bookingCollection.insertOne(bookingOrder);
             res.json(result)
         })
+        // DELETE API
+        app.delete('/deleteBooking/:id',async(req,res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)}
+            const result = bookingCollection.deleteOne(query)
+            res.json(result)
+        })
+        // delete Manage Users
+        app.delete('/deleteManageUsers/:id',async(req,res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)}
+            const result = await bookingCollection.deleteOne(query)
+            console.log(result);
+            res.send(result)
+        })
+        // update Status
+        app.put('/updateStatus/:id',async(req,res) => {
+            const id = req.params.id;
+            const filter = {_id:ObjectId(id)}
+            const updateStatus = await bookingCollection.findOne(filter);
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                  status:updateStatus.status = 'Approved'
+                }
+              };
+            const result = await bookingCollection.updateOne(filter,updateDoc,options)
+            // console.log(result);
+            res.send(result)
+        })
+       
     }
-    finally{
+    finally {
         // await client.close()
     }
 }
 run().catch(console.dir);
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send('Delivery server running')
 })
 
-app.listen(process.env.PORT || 5000,() => {
-    console.log('server running',port);
+app.listen(process.env.PORT || 5000, () => {
+    console.log('server running', port);
 })
